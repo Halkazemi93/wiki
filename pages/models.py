6e922f130b257c10a6b41a5dcb17808f91263f4c
Hussain Al Kazemi <mhk.alk@gmail.com>
2019-10-02 20:52:02 +0300

    Wiki project completed

--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,11 @@
 from django.db import models
-
 # Create your models here.
+
+class Page(models.Model):
+	title = models.CharField(max_length=105)
+	content = models.TextField()
+	last_update = models.DateTimeField(auto_now=True)
+
+	def get_absolute_url(self):
+		# from django.urls import reverse
+		return "/detail/%i" % self.id
--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from .models import Page
 
+
+def page_list(request):
+	context = {
+		"title": Page.objects.all(),
+	}
+	return render(request, 'list.html', context)
+
+
+def page_detail(request, page_id):
+	context = {
+		"title": Page.objects.get(id=page_id),
+	}
+	return render(request, 'detail.html', context)
 # Create your views here.
